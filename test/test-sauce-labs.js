a5a8c588c6af64ef318fb4f4f2df76da81cc81a9
Chasen Le Hara <chasen@chasenlehara.com>
2017-11-02 15:28:19 -0700

    Add the other browsers for Sauce Labs testing

--- test/test-sauce-labs.js
+++ test/test-sauce-labs.js
@@ -10,6 +10,14 @@ var platforms = [{
 	version: 'latest'
 }, {
 	browserName: 'internet explorer',
+	platform: 'Windows 7',
+	version: '9'
+}, {
+	browserName: 'internet explorer',
+	platform: 'Windows 8',
+	version: '10.0'
+}, {
+	browserName: 'internet explorer',
 	platform: 'Windows 10',
 	version: '11.0'
 }, {
@@ -23,6 +31,12 @@ var platforms = [{
 	browserName: 'firefox',
 	platform: 'Windows 10',
 	version: 'latest'
+}, {
+	browserName: 'Safari',
+	'appium-version': '1.7.1',
+	platformName: 'iOS',
+	platformVersion: '11.0',
+	deviceName: 'iPhone 8 Simulator'
 }];
 
 var url = 'http://canjs.test:3000/test/index.html?hidepassed';
